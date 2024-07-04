43bb868162a605e665b45d6c4eaee7c5b4077ea3
reechaaaa <richagoyal3142@gmail.com>
2024-07-04 14:57:23 +0530

    m

---
+++ frontPage/signupPage/src/mongodb.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://localhost:27017/loginSignuptutorial")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log("failed to connect");
+})
+
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection=new mongoose.model("Collection2",LogInSchema)
+
+module.exports=collection
